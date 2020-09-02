a54bfe333a6e3c8e22a343a9527937b0549709b1
sebasfernandezr <sebas.fernandezr@hotmail.com>
2020-09-01 21:12:52 -0500

    p

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "flutter demo",
+      title: "flutter p",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
