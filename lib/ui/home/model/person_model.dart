91dedc99730f1d5d8d3d215ca686552a04141bc6
sebasfernandezr <sebas.fernandezr@hotmail.com>
2020-09-03 19:49:36 -0500

    modelperson

---
+++ lib/ui/home/model/person_model.dart
@@ -0,0 +1,50 @@
+class PersonModel {
+  PersonModel({
+    this.key,
+    this.name,
+    this.phone,
+    this.isDeleted,
+    this.deletedDate,
+    this.createdDate,
+  });
+
+  String key;
+  String name;
+  String phone;
+  bool isDeleted;
+  Timestamp deletedDate;
+  Timestamp createdDate;
+
+  factory PersonModel.fromSnapshot(DocumentSnapshot snapshot) {
+    return PersonModel(
+      key: snapshot.id,
+      name: snapshot.data()["name"],
+      phone: snapshot.data()["phone"],
+      isDeleted: snapshot.data()["isDeleted"],
+      deletedDate: snapshot.data()["deleteDate"],
+      createdDate: snapshot.data()["createdDate"],
+    );
+  }
+
+  toJson() {
+    return {
+      "key": key,
+      "name": name,
+      "phone": phone,
+      "isDeleted": isDeleted,
+      "deleteDate": deletedDate,
+      "createDate": createdDate,
+    };
+  }
+
+factory PersonModel.fromJson(parsedJson) {
+    return PersonModel(
+      key: parsedJson.id,
+      name: parsedJson.data()["name"],
+      phone: parsedJson.data()["phone"],
+      isDeleted: parsedJson.data()["isDeleted"],
+      deletedDate: parsedJson.data()["deleteDate"],
+      createdDate: parsedJson.data()["createdDate"],
+    );
+
+}
